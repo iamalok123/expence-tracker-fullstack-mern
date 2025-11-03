85a7291a32a16cb1f3fa5338f68671de4298e315
Alok Hotta <alokhotta10@gmail.com>
2025-11-03 14:35:23 +0530

    bug fix style

--- Frontend/expense-tracker/src/pages/Auth/Signup.jsx
+++ Frontend/expense-tracker/src/pages/Auth/Signup.jsx
@@ -92,7 +92,7 @@ const Signup = () => {
         <form onSubmit={handleSignUp}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
 
             <Input
               value={fullName}
@@ -108,7 +108,7 @@ const Signup = () => {
               placeholder='alok@gmail.com'
               type="email"
             />
-            <div className='col-span-2'>
+            <div className='lg:col-span-2'>
               <Input
                 value={password}
                 onChange={({ target }) => setPassword(target.value)}
